69e46ad37218eeed78e113c43f9ea81f31936832
frogtech0 <techfrog0@gmail.com>
2023-01-07 04:29:07 +0100

    feat(twitter): added twitter icon to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import Github from "../public/github.svg"
+import Twitter from "../public/twitter.svg"
 
 function FooterComp(){
     return (<footer className='text-center p-10'>
@@ -10,6 +11,11 @@ function FooterComp(){
           <div>
             <Link href="https://github.com/fr0gtech">
               <Github className="opacity-30" fill="#fff" width={18} height={18} />
+            </Link>
+          </div>
+          <div>
+            <Link href="https://twitter.com/frogtechdev">
+              <Twitter className="opacity-30" fill="#fff" width={18} height={18} />
             </Link>
           </div>
         </div>
